26defbaf253fc7d499b41a25b2dbcf442d3dec5a
kimdodo97 <eh4536@naver.com>
2024-08-08 00:11:39 +0900

    Fix : 헤더 로그아웃 빈칸 설정 수정

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -14,32 +14,35 @@ export const Header = () => {
 	const logoutHandler = () => {
 		setAccessToken(null);
 		setRefreshToken(null);
-	}
+	};
 
 	const showLogoutIcon = accessToken && refreshToken; // 액세스 토큰과 리프레시 토큰이 모두 존재할 때
 
 	return (
 		<div>
 			<div className="flex justify-between h-16 bg-white border-b border-D9D9D9">
-				{showLogoutIcon && (
-					<div className="flex justify-center items-center">
+				{/* 로그아웃 아이콘이 있는 경우와 없는 경우 모두를 처리 */}
+				<div className="flex justify-center items-center w-16">
+					{showLogoutIcon ? (
 						<img
 							onClick={logoutHandler}
 							src={headerIcon.logoutIcon}
 							alt="로그아웃 아이콘"
-							className="w-6 h-6 ml-3 cursor-pointer"
+							className="w-6 h-6 cursor-pointer"
 						/>
-					</div>
-				)}
+					) : (
+						<div className="w-6 h-6" />
+					)}
+				</div>
 				<div className="flex justify-items-center items-center text-center font-bold text-lg">
 					헤더입니다
 				</div>
-				<div className="flex justify-center items-center">
+				<div className="flex justify-center items-center mr-3">
 					<img
 						onClick={() => onClick("/alarm-history")}
 						src={headerIcon.bellIcon}
 						alt="종 모양 아이콘"
-						className="w-6 h-6 mr-3 cursor-pointer"
+						className="w-6 h-6 cursor-pointer"
 					/>
 				</div>
 			</div>
