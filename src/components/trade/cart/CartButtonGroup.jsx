6754124a0de3853913577c19dab8fc7ab9b6e5a5
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2024-08-09 09:58:42 +0900

    Feat : 장바구니 관련 화면 구현  (#10)
    * Feat : 장바구니 상품 목록 컴포넌트
    
    * Feat : 현재 담긴 브로커 정보 추가
    
    * Chroe : 리액트 버전 수전
    
    * Feat : 출고 요청 시 최종 확인 모달 창 추가

--- src/components/trade/cart/CartButtonGroup.jsx
+++ src/components/trade/cart/CartButtonGroup.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import OutgoingModal from './OutgoingModal,';
 
 const CartButtonGroup = () => {
   const navigate = useNavigate();   
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleAddProduct = () => {
     // navigate(`/trades/broker/${company.id}`  , { state: { company } });
     navigate("/trades")
   }
 
+   const handleOpenModal = () => {
+    setIsModalOpen(true);  // Show the modal
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);  // Hide the modal
+  };
+
   return (
     <div className='flex flex-col gap-2 border-t-2 mx-2 bg-white h-full'>
       <button className='w-full border border-black p-2 rounded-md font-bold' onClick={handleAddProduct}>+ 상품 추가</button>
-      <button className='w-full border border-blue-600 bg-blue-600 text-white p-2 rounded-md font-bold'>도움 요청</button>
+      <button className='w-full border border-blue-600 bg-blue-600 text-white p-2 rounded-md font-bold' onClick={handleOpenModal}>도움 요청</button>
+      <OutgoingModal isOpen={isModalOpen} onClose={handleCloseModal} />
     </div>
   );
 };
