f698b8ce7798a52db121dfc5bce4a5938131a4b0
kimdodo97 <eh4536@naver.com>
2024-08-11 23:18:45 +0900

    Rename : 인덴트 조절 및 주석 삭제

--- src/components/trade/cart/CartButtonGroup.jsx
+++ src/components/trade/cart/CartButtonGroup.jsx
@@ -5,25 +5,26 @@ import OutgoingModal from './OutgoingModal,';
 const CartButtonGroup = () => {
   const navigate = useNavigate();   
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  
   const handleAddProduct = () => {
-    // navigate(`/trades/broker/${company.id}`  , { state: { company } });
     navigate("/trades")
   }
 
    const handleOpenModal = () => {
-    setIsModalOpen(true);  // Show the modal
-  };
+    setIsModalOpen(true);  
 
   const handleCloseModal = () => {
-    setIsModalOpen(false);  // Hide the modal
+    setIsModalOpen(false);  
   };
 
   return (
     <div className='flex flex-col gap-2 border-t-2 mx-2 bg-white h-full'>
       <button className='w-full border border-black p-2 rounded-md font-bold' onClick={handleAddProduct}>+ 상품 추가</button>
       <button className='w-full border border-blue-600 bg-blue-600 text-white p-2 rounded-md font-bold' onClick={handleOpenModal}>도움 요청</button>
-      <OutgoingModal isOpen={isModalOpen} onClose={handleCloseModal} />
+      <OutgoingModal
+        isOpen={isModalOpen} 
+        onClose={handleCloseModal}
+      />
     </div>
   );
 };
