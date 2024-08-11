3e94a6b4453edfdb23a3c3375dbab8f5f7ce5b24
kimdodo97 <eh4536@naver.com>
2024-08-12 00:20:45 +0900

    Feat : 뒤로 가기 아이콘 추가

--- src/constants/header/header.image.js
+++ src/constants/header/header.image.js
@@ -1,10 +1,12 @@
 import bellIcon from "../../assets/header/bell-icon.svg";
 import boxIcon from "../../assets/header/box-icon.svg";
 import logoutIcon from "../../assets/header/logout-icon.svg"
+import backIcon from "../../assets/header/back-icon.svg"
 const headerIcon = {
 	bellIcon,
 	boxIcon,
 	logoutIcon,
+	backIcon
 };
 
 export default headerIcon;
