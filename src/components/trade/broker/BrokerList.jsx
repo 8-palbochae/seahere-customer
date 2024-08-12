164e93fdfa3ffe3b3a59bd73c07799799c56eca0
Jeong gi seok <wjdrltjr5@naver.com>
2024-08-12 13:14:39 +0900

    Fix : axios Instance

--- src/components/trade/broker/BrokerList.jsx
+++ src/components/trade/broker/BrokerList.jsx
@@ -1,83 +1,91 @@
-import React, { useEffect, useRef, useState } from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import BrokerInfo from './BrokerInfo';
-import { url } from '../../../constants/defaultUrl';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import React, { useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import BrokerInfo from "./BrokerInfo";
+import { url } from "../../../constants/defaultUrl";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { axiosInstance } from "../../../api/common/axiosInstance";
 
-const fetchCompanies = async ({ pageParam = 1, size = 10, searchWord = "" }) => {
-  const response = await axios.get(`${url}/companies`, {
-    params: { page: pageParam, size, searchWord },
-  });
-  return response.data;
+const fetchCompanies = async ({
+	pageParam = 1,
+	size = 10,
+	searchWord = "",
+}) => {
+	const response = await axiosInstance.get(`${url}/companies`, {
+		params: { page: pageParam, size, searchWord },
+	});
+	return response.data;
 };
 
-const BrokerList = ({ searchQuery="", size = 10 }) => {
-  const [currentSearchTerm, setCurrentSearchTerm] = useState(searchQuery);
-  const loadMoreRef = useRef(null);
+const BrokerList = ({ searchQuery = "", size = 10 }) => {
+	const [currentSearchTerm, setCurrentSearchTerm] = useState(searchQuery);
+	const loadMoreRef = useRef(null);
 
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    status,
-  } = useInfiniteQuery({
-    queryKey: ['brokerList', size, currentSearchTerm],
-    queryFn: ({ pageParam = 1 }) => fetchCompanies({ pageParam, size, searchWord: currentSearchTerm }),
-    getNextPageParam: (lastPage, pages) => {
-      return lastPage.length === size ? pages.length + 1 : undefined;
-    },
-  });
+	const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+		useInfiniteQuery({
+			queryKey: ["brokerList", size, currentSearchTerm],
+			queryFn: ({ pageParam = 1 }) =>
+				fetchCompanies({
+					pageParam,
+					size,
+					searchWord: currentSearchTerm,
+				}),
+			getNextPageParam: (lastPage, pages) => {
+				return lastPage.length === size ? pages.length + 1 : undefined;
+			},
+		});
 
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '20px',
-      threshold: 1.0,
-    };
-    const observer = new IntersectionObserver(handleObserver, options);
-    if (loadMoreRef.current) {
-      observer.observe(loadMoreRef.current);
-    }
-    return () => {
-      if (loadMoreRef.current) {
-        observer.unobserve(loadMoreRef.current);
-      }
-    };
-  }, [loadMoreRef.current, hasNextPage]);
+	useEffect(() => {
+		const options = {
+			root: null,
+			rootMargin: "20px",
+			threshold: 1.0,
+		};
+		const observer = new IntersectionObserver(handleObserver, options);
+		if (loadMoreRef.current) {
+			observer.observe(loadMoreRef.current);
+		}
+		return () => {
+			if (loadMoreRef.current) {
+				observer.unobserve(loadMoreRef.current);
+			}
+		};
+	}, [loadMoreRef.current, hasNextPage]);
 
-  const handleObserver = (entries) => {
-    const target = entries[0];
-    if (target.isIntersecting && hasNextPage) {
-      fetchNextPage();
-    }
-  };
+	const handleObserver = (entries) => {
+		const target = entries[0];
+		if (target.isIntersecting && hasNextPage) {
+			fetchNextPage();
+		}
+	};
 
-  useEffect(() => {
-    setCurrentSearchTerm(searchQuery);
-  }, [searchQuery]);
+	useEffect(() => {
+		setCurrentSearchTerm(searchQuery);
+	}, [searchQuery]);
 
-  if (status === 'loading') return <p>Loading...</p>;
-  if (status === 'error') return <p>Error loading data.</p>;
+	if (status === "loading") return <p>Loading...</p>;
+	if (status === "error") return <p>Error loading data.</p>;
 
-  const companies = data?.pages.flatMap(page => page) || [];
+	const companies = data?.pages.flatMap((page) => page) || [];
 
-  return (
-    <div className="flex flex-col items-center my-2 w-11/12">
-      {companies.map(company => (
-        <BrokerInfo key={company.id} company={company} />
-      ))}
-      <div ref={loadMoreRef} className="h-10 flex justify-center items-center">
-        {isFetchingNextPage && <p>Loading more...</p>}
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center my-2 w-11/12">
+			{companies.map((company) => (
+				<BrokerInfo key={company.id} company={company} />
+			))}
+			<div
+				ref={loadMoreRef}
+				className="h-10 flex justify-center items-center"
+			>
+				{isFetchingNextPage && <p>Loading more...</p>}
+			</div>
+		</div>
+	);
 };
 
 BrokerList.propTypes = {
-  searchQuery: PropTypes.string.isRequired, 
-  size: PropTypes.number,
+	searchQuery: PropTypes.string.isRequired,
+	size: PropTypes.number,
 };
 
 export default BrokerList;
--- src/components/trade/cart/CartItemList.jsx
+++ src/components/trade/cart/CartItemList.jsx
@@ -1,94 +1,114 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import CartItem from './CartItem';
-import useCartStore from '../../../stores/cart';
-import { url } from '../../../constants/defaultUrl';
-import { tradeIcon } from '../../../constants/trade/trade.image';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import CartItem from "./CartItem";
+import useCartStore from "../../../stores/cart";
+import { url } from "../../../constants/defaultUrl";
+import { tradeIcon } from "../../../constants/trade/trade.image";
+import { axiosInstance } from "../../../api/common/axiosInstance";
 
 const CartItemList = () => {
-    const { cartItems, company } = useCartStore((state) => ({
-        cartItems: state.cartItems,
-        company : state.company
-    }));
+	const { cartItems, company } = useCartStore((state) => ({
+		cartItems: state.cartItems,
+		company: state.company,
+	}));
 
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [error, setError] = useState(null);
-    const [companyData, setCompanyData] = useState(null); // State to store the fetched data
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [error, setError] = useState(null);
+	const [companyData, setCompanyData] = useState(null); // State to store the fetched data
 
-    useEffect(() => {
-        console.log(`${url}/companies/${company}`);
-        if (company) {
-            const fetchData = async () => {
-                try {
-                    const response = await fetch(`${url}/companies/${company}`);
-                    if (response.status === 200) {
-                        const data = await response.json();
-                        console.log(data);
-                        setCompanyData(data);  // Store the fetched data in state
-                        setIsLoaded(true);
-                    } else {
-                        throw new Error('Failed to load data');
-                    }
-                } catch (err) {
-                    setError(err.message);
-                    setIsLoaded(false);
-                }
-            };
+	useEffect(() => {
+		console.log(`${url}/companies/${company}`);
+		if (company) {
+			const fetchData = async () => {
+				try {
+					const response = await axiosInstance.get(
+						`${url}/companies/${company}`
+					);
+					if (response.status === 200) {
+						const data = response.data;
+						setCompanyData(data); // Store the fetched data in state
+						setIsLoaded(true);
+					} else {
+						throw new Error("Failed to load data");
+					}
+				} catch (err) {
+					setError(err.message);
+					setIsLoaded(false);
+				}
+			};
 
-            fetchData();
-        }
-    }, [company]);
+			fetchData();
+		}
+	}, [company]);
 
-    const totalShipmentAmount = cartItems.reduce((total, cartItem) => {
-        return total + (cartItem.price || 0);
-    }, 0);
+	const totalShipmentAmount = cartItems.reduce((total, cartItem) => {
+		return total + (cartItem.price || 0);
+	}, 0);
 
-    const totalQuantity = cartItems.reduce((total, cartItem) => {
-        return total + (cartItem.quantity || 0);
-    }, 0);
+	const totalQuantity = cartItems.reduce((total, cartItem) => {
+		return total + (cartItem.quantity || 0);
+	}, 0);
 
-    if (error) {
-        return <div className='text-red-500'>Error: {error}</div>;
-    }
+	if (error) {
+		return <div className="text-red-500">Error: {error}</div>;
+	}
 
-    if (!isLoaded) {
-        return <div>Loading...</div>; // Show a loading state while the request is being processed
-    }
+	if (!isLoaded) {
+		return <div>Loading...</div>; // Show a loading state while the request is being processed
+	}
 
-    return (
-        <>
-            <div className='flex w-full text-left font-bold text-xl ml-4 mt-4 mb-2 justify-start gap-3 items-center'>
-                 <div className='w-6 h-6'>
-                    <img src={tradeIcon.brokerLogo} alt="" className='w-full h-full object-cover rounded-md' />
-                </div>
-                <span>{companyData.companyName}</span>
-            </div>
-            <div className='flex flex-col items-center p-2'>
-                {cartItems.length > 0 ? (
-                    cartItems.map((cartItem) => (
-                        <CartItem key={cartItem.id} cartItem={cartItem}/>
-                    ))
-                ) : (
-                    <div className='text-gray-500'>장바구니에 항목이 없습니다.</div>
-                )}
-                <div className='font-bold w-full text-left text-lg ml-2 mt-4 mb-2'>출고 요청을 확인 해주세요</div>
-                <div className='w-full items-center p-4 border rounded-md shadow-md'>
-                    <div className='flex justify-between border-b my-2 pb-4'>
-                        <div className='font-semibold'>총 출고량</div>
-                        <div><span className='text-blue-600 font-semibold'>{totalQuantity}</span> Kg</div>
-                    </div>
-                    <div className='flex justify-between my-2 pt-2'>
-                        <div className='font-semibold'>총 출고 금액</div>
-                        <div><span className='text-blue-600 font-semibold'>{totalShipmentAmount.toLocaleString()}</span> 원</div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="flex w-full text-left font-bold text-xl ml-4 mt-4 mb-2 justify-start gap-3 items-center">
+				<div className="w-6 h-6">
+					<img
+						src={tradeIcon.brokerLogo}
+						alt=""
+						className="w-full h-full object-cover rounded-md"
+					/>
+				</div>
+				<span>{companyData.companyName}</span>
+			</div>
+			<div className="flex flex-col items-center p-2">
+				{cartItems.length > 0 ? (
+					cartItems.map((cartItem) => (
+						<CartItem key={cartItem.id} cartItem={cartItem} />
+					))
+				) : (
+					<div className="text-gray-500">
+						장바구니에 항목이 없습니다.
+					</div>
+				)}
+				<div className="font-bold w-full text-left text-lg ml-2 mt-4 mb-2">
+					출고 요청을 확인 해주세요
+				</div>
+				<div className="w-full items-center p-4 border rounded-md shadow-md">
+					<div className="flex justify-between border-b my-2 pb-4">
+						<div className="font-semibold">총 출고량</div>
+						<div>
+							<span className="text-blue-600 font-semibold">
+								{totalQuantity}
+							</span>{" "}
+							Kg
+						</div>
+					</div>
+					<div className="flex justify-between my-2 pt-2">
+						<div className="font-semibold">총 출고 금액</div>
+						<div>
+							<span className="text-blue-600 font-semibold">
+								{totalShipmentAmount.toLocaleString()}
+							</span>{" "}
+							원
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 CartItemList.propTypes = {
-    // Define prop types if necessary
+	// Define prop types if necessary
 };
 
 export default CartItemList;
