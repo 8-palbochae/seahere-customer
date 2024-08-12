a4f315a8b04de80ca2860f76dbd6c1d7d6f80ea0
kimdodo97 <eh4536@naver.com>
2024-08-12 09:39:06 +0900

    Feat : 출고 요청 조회 검색 창 추가

--- src/components/outgoing/page/OutgoingList.jsx
+++ src/components/outgoing/page/OutgoingList.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import OutgoingListItem from '../items/OutgoingListItem';
-import BrokerSearchInput from '../../common/BrokerSearchInput';
+import OutgoingSearch from '../OutgoingSearch';
 
 const OutgoingList = () => {
+    const {searchWord, setSearchWord} = useState('');
+
     return (
         <div className='w-full'>
+            <OutgoingSearch searchWord={searchWord} setSearchWord={setSearchWord}/>
             <OutgoingListItem/>  
             <div className='h-3 bg-gray-200'></div>
             <OutgoingListItem/>  
             <div className='h-3 bg-gray-200'></div>
             <OutgoingListItem/>  
             <div className='h-3 bg-gray-200'></div>
+            <OutgoingListItem/>  
+            <div className='h-3 bg-gray-200'></div> 
+            <OutgoingListItem/>  
+            <div className='h-3 bg-gray-200'></div> 
             <OutgoingListItem/>  
             <div className='h-3 bg-gray-200'></div> 
         </div>
