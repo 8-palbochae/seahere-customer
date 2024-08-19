c1bf81b12bf246b6b8d25cc07db5a5eaa6ab18a1
kimdodo97 <eh4536@naver.com>
2024-08-19 22:27:06 +0900

    Fix : 컴포넌트 인덱스 수정

--- src/components/setting/index.js
+++ src/components/setting/index.js
@@ -1,3 +1,1 @@
-export { default as UserInfoSetting } from "./userinfo/UserInfoSetting";
-export { default as PasswordChange } from "./userinfo/password_change/PasswordChange";
-export { default as MainSetting } from "./main/MainSetting";
+export { default as SettingMain } from "./SettingMain";
