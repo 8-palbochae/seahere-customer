d06b30649f576696a1b78588e772b2a84a672b65
kimdodo97 <eh4536@naver.com>
2024-08-20 01:12:12 +0900

    Feat : 사용자 설정 변경 컴포넌트 추가

--- src/components/setting/index.js
+++ src/components/setting/index.js
@@ -1,1 +1,4 @@
 export { default as SettingMain } from "./SettingMain";
+export { default as EditTelNumber } from "./page/EditTelNumber";
+export { default as EditPassword } from "./page/EditPassword";
+export { default as EditAddress } from "./page/EditAddress";
