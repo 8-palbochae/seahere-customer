1b7801eb578c413026537353bfb31a50ceb7f9d2
woneveryady <woneveryday@naver.com>
2024-08-22 13:51:14 +0900

    fix: 도움 요청을 출고 요청으로 수정

--- src/components/trade/cart/CartButtonGroup.jsx
+++ src/components/trade/cart/CartButtonGroup.jsx
@@ -21,7 +21,7 @@ const CartButtonGroup = () => {
   return (
     <div className='flex flex-col gap-2 border-t-2 mx-2 bg-white h-full'>
       <button className='w-full border border-black p-2 rounded-md font-bold' onClick={handleAddProduct}>+ 상품 추가</button>
-      <button className='w-full border border-blue-600 bg-blue-600 text-white p-2 rounded-md font-bold' onClick={handleOpenModal}>도움 요청</button>
+      <button className='w-full border border-blue-600 bg-blue-600 text-white p-2 rounded-md font-bold' onClick={handleOpenModal}>출고 요청</button>
       <OutgoingModal
         isOpen={isModalOpen} 
         onClose={handleCloseModal}
